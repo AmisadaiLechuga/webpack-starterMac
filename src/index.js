bd17e4a34197363b24a5e1975b58b3e04587e523
Amisadai <amisadailechu@gmail.com>
2022-07-13 21:05:42 -0500

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
-import { saludar } from './js/componentes.js';
 import './styles.css';
+/* import { saludar } from './js/componentes.js'; */
+
+/* import { Todo } from './classes/todo.class.js';
+import { TodoList } from './classes/todo-list.class'; */
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 
 
 
-const nombre = 'Amisadai';
+/* const nombre = 'Amisadai';
 
-saludar ( nombre );
+saludar ( nombre ); */
+
+export const todoList = new TodoList();
+
+/* const tarea = new Todo('Aprender JavaScript');
+todoList.nuevoTodo( tarea );
+
+
+console.log( todoList ); */
+
+/* crearTodoHtml( tarea ); */
+
+
+/* localStorage.setItem('mi-key', 'ABC123');
+sessionStorage.setItem('mi-key', 'ABC123'); */
+
+/* todoList.todos.forEach( todo => crearTodoHtml( todo ) );   //otra forma de codigo al de abajo */
+todoList.todos.forEach( crearTodoHtml );
+
+
+/* newTodo.imprimirClase(); */
+
+console.log( 'todos', todoList.todos);
+
